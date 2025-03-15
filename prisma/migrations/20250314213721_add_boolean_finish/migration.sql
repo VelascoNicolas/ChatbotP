ac7254c0cdf29d2c47467ea15dcfa32c2ca1f21a
NicolasVelasco <nicolas.velasco@alumnos.frm.utn.edu.ar>
2025-03-14 22:18:53 -0300

    ready for deploy

---
+++ prisma/migrations/20250314213721_add_boolean_finish/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "messages" ADD COLUMN     "finishLane" BOOLEAN NOT NULL DEFAULT false;
--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -16,15 +16,15 @@ export class BotService implements OnModuleInit {
   });
   private enterpriseId: string = '';
   private prisma = new PrismaClient();
-
   private readonly logger = new Logger(BotService.name);
+  private followUpTimer: NodeJS.Timeout | null = null;
 
   constructor(private eventEmitter: EventEmitter2) {}
 
   onModuleInit() {
     this.client.on('qr', (qr) => {
       this.logger.log(
-        `QrCode: http://localhost:${3000}/bot/qrcode`,
+        `QrCode: http://localhost:${3000}/bot/qrcode/421a24c1-e02d-44b3-af45-267e1b5e306c`,
       );
       this.eventEmitter.emit('qrcode.created', qr);
     });
@@ -35,42 +35,57 @@ export class BotService implements OnModuleInit {
 
     let numOrderCount: number = 0;
     let idPadre: string = ' ';
+    let advance: number = 0;
 
     this.client.on('message', async (msg) => {
       this.logger.verbose(`${msg.from}: ${msg.body}`);
       const messages = await this.prisma.messages.findMany({where: {enterpriseId: this.enterpriseId}, orderBy: {numOrder: 'asc'}});
       let m;
+      if(msg.body === '1') {
+        advance = 1;
+        numOrderCount = numOrderCount+advance;
+      } else if(msg.body === '2') {
+        advance = 2;
+        numOrderCount = numOrderCount+advance;
+      } else if(msg.body === '3') {
+        advance = 3;
+        numOrderCount = numOrderCount+advance;
+      }
+
       for(let i = numOrderCount; i <= messages.length;) {
 
         if(messages[i]?.parentMessageId === null && messages[i]?.option === 'MENU') {
           m = messages[i]?.body;
           msg.reply(m);
           console.log('te respondi')
-          numOrderCount = numOrderCount+2;
           idPadre = messages[i]?.id;
           break;
         }
 
-        if(messages[i]?.parentMessageId === idPadre && messages[i]?.option === 'MENU' && messages[i]?.trigger === msg.body) {
+        if(messages[i]?.parentMessageId === idPadre && messages[i]?.option === 'MENU' && messages[i]?.trigger.toLowerCase() === msg.body.toLowerCase()) {
           console.log('entro?')
           const counti = await this.prisma.messages.count({where: {parentMessageId: messages[i]?.id}});
           const x = await this.prisma.messages.findUnique({where: {id: idPadre}});
           m = messages[i]?.body;
           msg.reply(m);
           idPadre = messages[i]?.id;
-          numOrderCount = (counti + x?.numOrder);
           break;
         }
 
-        if(messages[i]?.parentMessageId === idPadre && messages[i]?.trigger === msg.body) {
+        if(messages[i]?.parentMessageId === idPadre && messages[i]?.trigger.toLowerCase() === msg.body.toLowerCase()) {
           const counti = await this.prisma.messages.count({where: {parentMessageId: messages[i]?.id}});
           const x = await this.prisma.messages.findUnique({where: {id: idPadre}});
           m = messages[i]?.body;
           msg.reply(m);
           idPadre = messages[i]?.id;
-          numOrderCount = (counti + x?.numOrder) + 1;
           //numOrderCount = 0;
-          break;
+          if(messages[i]?.finishLane === true) {
+            numOrderCount = 0;
+            break;
+          } else {
+            break;
+          }
+
         }
 
         if(i == messages.length-1) {
@@ -83,6 +98,7 @@ export class BotService implements OnModuleInit {
         if(numOrderCount === 0) {
           m = messages[i]?.body;
           await msg.reply(m);
+          numOrderCount = numOrderCount+1;
         }
         i++;
       }
@@ -96,7 +112,6 @@ export class BotService implements OnModuleInit {
   }
 
   async disconnect() {
-    //const authPath = path.join(__dirname, '..', '..', '.wwebjs_auth');
     const cachePath = path.join(__dirname, '..', '..', '.wwebjs_cache');
 
     const deleteFolder = async (folderPath: string) => {
@@ -116,7 +131,6 @@ export class BotService implements OnModuleInit {
     };
 
     try {
-      //await deleteFolder(authPath);
       await deleteFolder(cachePath);
       await this.client.logout();
       this.logger.log('Client disconnected and folders deleted successfully.');
